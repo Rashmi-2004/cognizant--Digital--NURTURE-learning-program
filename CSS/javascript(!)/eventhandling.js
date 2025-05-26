cb0d0f9d6255ca3fcfa67b542cea3b379bb4a0cf
Rashmi-2004 <dyrapogurashmi@gmail.com>
2025-05-26 22:58:05 +0530

    Create eventhandling.js

---
+++ CSS/javascript(!)/eventhandling.js
@@ -0,0 +1,8 @@
+<select id="categoryFilter">
+  <option value="All">All</option>
+  <option value="Music">Music</option>
+  <option value="Education">Education</option>
+</select>
+
+<input type="text" id="searchBox" placeholder="Search events" />
+<div id="eventList"></div>
