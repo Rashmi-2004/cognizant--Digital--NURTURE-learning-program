a6bd77feea08147214c321264136014d87d40130
Rashmi-2004 <dyrapogurashmi@gmail.com>
2025-05-26 22:54:23 +0530

    Create functions.js

---
+++ CSS/javascript(!)/functions.js
@@ -0,0 +1,24 @@
+function addEvent(name, date, category) {
+  return { name, date, category };
+}
+
+function registerUser(event) {
+  console.log(`User registered for ${event.name}`);
+}
+
+function filterEventsByCategory(events, category) {
+  return events.filter(e => e.category === category);
+}
+
+// Closure to track total registrations per category
+function registrationTracker() {
+  let count = 0;
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const musicTracker = registrationTracker();
+console.log(musicTracker());
+console.log(musicTracker());
