89f0ae8ae12af18bca18bc4f7b9390f3907f3f86
Rashmi-2004 <dyrapogurashmi@gmail.com>
2025-05-26 23:10:14 +0530

    Delete CSS directory

--- CSS/javascript(!)/Operators.js
+++
@@ -1,8 +0,0 @@
-const eventName = "Music Fest";
-const eventDate = "2025-06-10";
-let seatsAvailable = 50;
-
-console.log(`Event: ${eventName}, Date: ${eventDate}, Seats: ${seatsAvailable}`);
-
-seatsAvailable--;
-console.log(`After one registration, remaining seats: ${seatsAvailable}`);
--- CSS/javascript(!)/eventhandling.js
+++
@@ -1,8 +0,0 @@
-<select id="categoryFilter">
-  <option value="All">All</option>
-  <option value="Music">Music</option>
-  <option value="Education">Education</option>
-</select>
-
-<input type="text" id="searchBox" placeholder="Search events" />
-<div id="eventList"></div>
