9986c93dcc12accde4c2db7c7db5c0021535b339
Rashmi-2004 <dyrapogurashmi@gmail.com>
2025-05-26 22:52:22 +0530

    Create Operators.js

---
+++ CSS/javascript(!)/Operators.js
@@ -0,0 +1,8 @@
+const eventName = "Music Fest";
+const eventDate = "2025-06-10";
+let seatsAvailable = 50;
+
+console.log(`Event: ${eventName}, Date: ${eventDate}, Seats: ${seatsAvailable}`);
+
+seatsAvailable--;
+console.log(`After one registration, remaining seats: ${seatsAvailable}`);
