15fd27bb09753f45d6b71211ecd3e38c91cf6372
Rashmi-2004 <dyrapogurashmi@gmail.com>
2025-05-26 23:36:45 +0530

    Create Dom manulation.js

---
+++ CSS/JavaScript(1)/Dom manulation.js
@@ -0,0 +1,18 @@
+<div id="eventsContainer"></div>
+main.js
+
+javascript
+Copy
+Edit
+const events = [
+  { name: "Music", date: "2025-06-10" },
+  { name: "Yoga", date: "2025-06-15" }
+];
+
+const container = document.querySelector("#eventsContainer");
+
+events.forEach(event => {
+  const card = document.createElement("div");
+  card.textContent = `${event.name} on ${event.date}`;
+  container.appendChild(card);
+});
